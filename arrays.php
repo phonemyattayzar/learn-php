c46421014aeb17955ff8addd4f0b96bf6effcb30
Phone Myat <pmtzk2000@gmail.com>
2020-01-18 12:22:29 +0630

    Array in php

---
+++ arrays.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Arrays</title>
+</head>
+<body>
+
+<?php 
+$numberList = array(100,200,560,"phone myat","<h1>hello world</h1>");
+
+// $testList = ["john"];
+
+// echo $testList[0];
+
+print_r($numberList);
+
+echo $numberList[3];
+
+
+
+
+
+?>
+    
+</body>
+</html>
