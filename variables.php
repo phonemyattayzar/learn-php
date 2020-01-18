06788c7cf0beb8b82527abdf35609c398a3b22d6
Phone Myat <pmtzk2000@gmail.com>
2020-01-18 10:12:52 +0630

    Math in php

---
+++ variables.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Variable</title>
+</head>
+<body>
+
+    <?php 
+
+        $name = "phone myat";
+        $number = 100;
+        $NUMBER = 100;
+        $NUMBeR = 50;
+        $test = "<h1> Hello World</h1>";
+
+        echo $test;
+
+        //String concatenation
+        echo $name . " " . $NUMBeR;
+    
+    
+    
+    
+    ?>
+    
+</body>
+</html>
